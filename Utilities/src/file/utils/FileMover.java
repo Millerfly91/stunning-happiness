6ce7db5a6a886a7e67783698963d94e089568052
Millerfly91 <32804913+Millerfly91@users.noreply.github.com>
2018-10-19 19:35:35 -0400

    Update Utilities/src/file/utils/FileMover.java

--- Utilities/src/file/utils/FileMover.java
+++ Utilities/src/file/utils/FileMover.java
@@ -5,10 +5,26 @@
  */
 package file.utils;
 
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+
 /**
  *
  * @author James
  */
 public class FileMover {
+    
+    public static void main(String args[]) throws IOException {
+        File source = new File("C:\\James44\\Nuts\\morenuts\\thosenuts.txt");
+        File dest = new File("C:\\James45\\deweper\\anothewrone");
+        
+        copyFiles(source, dest);
+    }
+    
+    public static void copyFiles(File source, File dest) throws IOException {
+        dest.mkdirs();
+        FileUtils.copyFileToDirectory(source, dest);
+    }
     
 }
