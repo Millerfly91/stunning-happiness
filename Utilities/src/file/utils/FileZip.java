357b23f5ccc73caea280ff7d8b67b5e00fd838c9
Millerfly91 <32804913+Millerfly91@users.noreply.github.com>
2018-10-27 13:21:26 -0400

    Update Utilities/src/file/utils/FileZip.java
    created new zip util

---
+++ Utilities/src/file/utils/FileZip.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package file.utils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+/**
+ *
+ * @author James
+ */
+public class FileZip {
+    
+    public static void main(String args[]){
+        
+    }
+    
+    public static File zipFile(File filetozip){
+    
+}
+    
+}
