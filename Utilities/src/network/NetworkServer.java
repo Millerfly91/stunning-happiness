5c04fe2e8de982e0d6d099704fd284a57117bfef
JLRoper <JacobLRoper@gmail.com>
2018-11-11 17:50:48 -0500

    command line method

--- Utilities/src/network/NetworkServer.java
+++ Utilities/src/network/NetworkServer.java
@@ -26,16 +26,22 @@ public class NetworkServer {
 
     ServerSocket sockServ;
     BlockingQueue<Runnable> runQueue;
-    ThreadPoolExecutor tpe;
+    ThreadPoolExecutor threadExecutor;
+    ConnectionAction connectionAction;
 
     public static void main(String[] argv) {
-        NetworkServer testServer = new NetworkServer();
-        testServer.listen(1109);
+//        NetworkServer testServer = new NetworkServer();
+//        testServer.listen(1109);
     }
 
     public NetworkServer() {
         this.runQueue = new LinkedBlockingQueue<>();
-        this.tpe = new ThreadPoolExecutor(0, 3, 10, TimeUnit.MINUTES, runQueue);
+        this.threadExecutor = new ThreadPoolExecutor(0, 3, 10, TimeUnit.MINUTES, runQueue);
+    }
+
+    public NetworkServer setAction(ConnectionAction connectionAction) {
+        this.connectionAction = connectionAction;
+        return this;
     }
 
     public void listen(int port) {
@@ -59,10 +65,10 @@ public class NetworkServer {
     }
 
     protected void startNextWorker() {
-        ClientWorker cw = new ClientWorker();
+        ClientWorker cw = new ClientWorker(connectionAction);
 //        runQueue.add(cw);
-        tpe.execute(cw);
-        
+        threadExecutor.execute(cw);
+
     }
 
     protected Socket getSocketAndProcess() throws IOException {
@@ -74,6 +80,20 @@ public class NetworkServer {
 
         BufferedReader dataIn;
         PrintWriter dataOut;
+        ConnectionAction action;
+//                = (sock) -> {
+//            try {
+//                String recievedData = processConnection(sock);
+//                System.out.println("Recieved from client: " + recievedData);
+//                writeDataOut(sock, recievedData.toUpperCase());
+//            } catch (Throwable t) {
+//                t.printStackTrace();
+//            }
+//        };
+
+        public ClientWorker(ConnectionAction action) {
+            this.action = action;
+        }
 
         @Override
         public void run() {
@@ -81,25 +101,55 @@ public class NetworkServer {
                 System.out.println("Starting up client handler.");
                 Socket sock = getSocketAndProcess();
                 startNextWorker();
-                String recievedData = processConnection(sock);
-                System.out.println("Recieved from client: " + recievedData);
-                writeDataOut(sock, recievedData.toUpperCase());
 
+                long timeToProcess = System.currentTimeMillis();
+                action.handleNewConnection(sock);
+                timeToProcess = System.currentTimeMillis() - timeToProcess;
+                System.out.println("Time to process connection: " + timeToProcess + " ms");
             } catch (Throwable ex) {
-                Logger.getLogger(NetworkServer.class.getName()).log(Level.SEVERE, "Error made it to top of worker.", ex);
+                Logger.getLogger(NetworkServer.class.getName()).log(Level.SEVERE,
+                        "Error made it to top of worker.", ex);
             }
         }
 
-        protected String processConnection(Socket sock) throws IOException {
-            dataIn = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+        protected String processConnection(Socket sock)
+                throws IOException {
+            dataIn = new BufferedReader(
+                    new InputStreamReader(sock.getInputStream()));
             return dataIn.readLine();
         }
 
-        protected void writeDataOut(Socket sock, String otuput) throws IOException {
+        protected void writeDataOut(Socket sock, String otuput)
+                throws IOException {
             dataOut = new PrintWriter(sock.getOutputStream(), true);
-            dataOut.println(otuput);
+            dataOut.println(otuput);    
         }
 
+    }
+
+    protected String executeCommandLine(String command) {
+
+        try {
+            // Just one line and you are done !  
+            // We have given a command to start cmd 
+            // /K : Carries out command specified by string 
+            Runtime.getRuntime().exec("cmd  /K \"powershell -Command "
+                    + "\"Start-Process 'cmd.exe' -Verb runAs -ArgumentList \"\"/k " + command+"\"\"\""
+                    + "\"");
+//            Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"" + command + "\"");
+
+        } catch (Exception e) {
+            System.out.println("HEY Buddy ! U r Doing Something Wrong ");
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
+    @FunctionalInterface
+    public interface ConnectionAction {
+
+        public abstract void handleNewConnection(Socket sock);
     }
 
 }
