b3d02bd38e2c5a9df6ab3b7b159638787d5c8f2d
Millerfly91 <32804913+Millerfly91@users.noreply.github.com>
2018-11-18 22:57:59 -0500

    Update TBD.java

--- Utilities/src/network/TBD.java
+++ Utilities/src/network/TBD.java
@@ -5,37 +5,76 @@
  */
 package network;
 
+import java.io.IOException;
+import java.util.List;
+import console.ScanIP;
+import network.NetworkServer.ConnectionAction;
+import network.connection.Connection;
+
 /**
  *
  * @author James
  */
 public class TBD {
+
+    String clientAddress = "192.168.1.10";
+    String port = "1109";
+    String keyword = "BERT";
+    NetworkServer server;
+    List validatedClients;
+    String message;
+    String port = "1109";
+    
+
     public static void main(String[] argv) {
-        startServer();
-        
-        connectClient();
+        TBD startserv = new TBD();
+        startserv.startServer();
+
+//        startserv.connectClient("192.168.1.10", "1109", "What up Ernie?");
     }
 
-    public static void startServer() {
-        NetworkServer testServ
+    public void startServer() {
+       ConnectionAction newConnectionAction = conn -> {
+            validateIncomingConnection(conn);
+        };
+        
+        server
                 = new NetworkServer().
-                        setAction(conn -> {
-                            try {
-                                String recievedData = conn.readAsString();
-                                System.out.println(recievedData);
-                                conn.sendString("TEST Out. Recieved = " + recievedData);
-                            } catch (Throwable t) {
-                                t.printStackTrace();
-                            }
-                        }).
+                        setAction(newConnectionAction).
                         setPort(1109).
                         start();
     }
-    
-    public static void connectClient() {
-            NetworkClient testInstance = new NetworkClient();
-            testInstance.connect("192.168.1.10", "1109");
-            testInstance.transmitStream("What up Ernie?");
+
+    public void validateIncomingConnection(Connection conn) {
+        try {
+            String recievedData = conn.readAsString();
+            
+            if (recievedData.startsWith(keyword)){
+                System.out.println(recievedData.substring(keyword.length()+1));
+                conn.sendString(keyword + " Recieved = " + recievedData);
+                
+            }
+                                    
+            
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
     }
     
+
+    public void connectClient(String clientAddress, String content) {
+        NetworkClient testInstance = new NetworkClient();
+        testInstance.connect(clientAddress, port);
+        testInstance.transmitStream(content);
+    }
+
+    public void findClient() throws IOException {
+        List<String> IPList = ScanIP.getActiveIPs();
+        TBD searchServ = new TBD();
+
+        for (String line  : IPList) {
+            searchServ.connectClient(line, keyword);
+        }
+    }
+
 }
