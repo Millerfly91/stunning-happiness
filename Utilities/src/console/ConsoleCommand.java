7f4206eb0778402cbb5a0c415c81d9ba5dbc3f22
JLRoper <JacobLRoper@gmail.com>
2018-11-21 23:55:53 -0500

    Update ConsoleCommand.java

--- Utilities/src/console/ConsoleCommand.java
+++ Utilities/src/console/ConsoleCommand.java
@@ -5,8 +5,10 @@
  */
 package console;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.util.List;
 import java.util.logging.Level;
@@ -17,37 +19,35 @@ import java.util.logging.Logger;
  * @author James
  */
 public class ConsoleCommand {
-    public static void main(String[] argv) throws IOException, InterruptedException {
-        
 
-        
+    public static void main(String[] argv) throws IOException, InterruptedException {
+
 //        another option
 //        results.forEach(System.out::println);
     }
 
     public static List runCommand(String command) throws IOException {
         File outputFile = new File("C:\\jakeTempFIle.txt");
-        if(outputFile.exists()!= true){
+        if (outputFile.exists() != true) {
             outputFile.createNewFile();
         }
         executeCommandLine("" + command + " > " + outputFile.getAbsolutePath() + " && exit && exit");
 
         try {
-            Thread.sleep(10*1000);
+            Thread.sleep(10 * 1000);
         } catch (Throwable ex) {
             Logger.getLogger(ConsoleCommand.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         List<String> fileLines = FileReader(outputFile);
 
         outputFile.delete();
         executeCommandLine("del " + outputFile + " && exit");
-        
+
 //        ---------   to print in this  -------------
 //        for (String line : fileLines) {
 //            System.out.println(line);
 //        }
-
         return fileLines;
     }
 
@@ -60,13 +60,25 @@ public class ConsoleCommand {
     }
 
     protected static String executeCommandLine(String command) {
-
         try {
-            Runtime.getRuntime().exec("powershell -Command "
-                    + "\"Start-Process \'cmd.exe\' -Verb runAs -ArgumentList \'/c  " + command + "\'\"");
+            Process proc
+                    = Runtime.getRuntime().exec("powershell -Command "
+                            + "\""
+                            /* Starts a command prompt. */
+                            + "Start-Process 'cmd.exe' "
+                            /* Start as administrator. */
+                            + "-Verb runAs "
+                            /* Line executed in command prompt. */
+                            + "-ArgumentList '/c  " + command + "'"
+                            + "\"");
+            BufferedReader reader
+                    = new BufferedReader(
+                            new InputStreamReader(
+                                    proc.getInputStream()));
 
+            
+            reader.lines().forEach(line -> System.out.println(line));
         } catch (Exception e) {
-            System.out.println("HEY Buddy ! U r Doing Something Wrong ");
             e.printStackTrace();
         }
         return "";
